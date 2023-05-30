4799d9d47a8dda8b27ff0e9403113dfb4ce97e6c
Radek Kozak <radoslaw.kozak@gmail.com>
2023-05-30 04:42:45 +0200

    Update configuration variables names
    Signed-off-by: Radek Kozak <radoslaw.kozak@gmail.com>

--- src/LetterSettings.js
+++ src/LetterSettings.js
@@ -3,17 +3,17 @@ import {config} from 'dotenv';
 config();
 
 const settings = {
-  opmlFile: (process.env.OPML_FEEDS || 'feeds.opml'),
+  opmlFile: (process.env.LETTER_FEEDS || 'plain'),
   email: {
-    from: process.env.EMAIL_FROM,
-    to: process.env.EMAIL_TO,
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    from: process.env.SMTP_FROM,
+    to: process.env.MAIL_TO,
   },
   smtp: {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     secure: process.env.SMTP_SECURE,
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
   },
 };
 
